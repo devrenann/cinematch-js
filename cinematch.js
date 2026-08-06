8ced5d5e0e1e6414bb825d372896e6a24095c2f9
devRenan <eurenanfroes@gmail.com>
2026-08-05 22:04:41 -0300

    chore: snapshot diario 2026-08-05
    Commit automatico do trabalho do dia (1 arquivo(s)), gerado por
    ops/git-daily-sync.sh. Revise e reescreva se precisar de historia limpa.

--- cinematch.js
+++ cinematch.js
@@ -1,5 +1,27 @@
 const prompt = require("prompt-sync")({sigint: true});
 
+class Conteudo {
+    constructor(titulo, tipo, generos, duracaoMinutos){
+        this.titulo = titulo;
+        this.tipo = tipo;
+        this.generos = generos;
+        this.duracaoMinutos = duracaoMinutos;
+    }
+    exibirResumo() {
+        return `${this.titulo} (${this.tipo}) - ${this.duracaoMinutos} min`;
+    }
+}
+
+class Serie extends Conteudo {
+    constructor(titulo, generos, duracaoMinutos, temporadas ) {
+        super(titulo, "Série", generos, duracaoMinutos);
+        this.temporadas = temporadas;
+    }
+    exibirTemporadas() {
+        return `${this.titulo} tem ${this.temporadas} temporadas(s)`;
+    }
+}
+
 console.log("- - -  CineMatch JS  - - -\n")
 
 const nome = prompt("Qual é o seu nome? - ")
@@ -18,3 +40,13 @@ const usuario = {
 
 console.log("\nPerfil criado com sucesso!");
 console.log(usuario);
+
+const serieTeste = new Serie(
+    "Fronteira Digital",
+    ["Ação", "Ficção Cientifica"],
+    45,
+    2
+);
+
+console.log(serieTest.exibirResumo())/
+console.log(serie
